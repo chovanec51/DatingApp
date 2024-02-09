5d4a622c99da69a63f1bddd5d330939e2e9b62c7
chovanec51 <chovanecadam0@gmail.com>
2024-02-09 19:44:13 +0100

    Walking skeleton - API

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Entities;
+
+//This class is an Entity. Entities represent tables in a db.
+public class AppUser    
+{
+    public int Id { get; set; } //property named Id is automatically used as a primary key in this table
+    public string UserName { get; set; }
+}
