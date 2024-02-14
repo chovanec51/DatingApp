01dc726242da7e69eff621de1232defc48ec7261
chovanec51 <chovanecadam0@gmail.com>
2024-02-14 19:10:23 +0100

    Added authentication and authorization

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController : BaseApiController
+{
+
+    private readonly DataContext _context;
+    private readonly ITokenService _tokenService;
+    public AccountController(DataContext context, ITokenService tokenService)
+    {
+        _context = context;
+        _tokenService = tokenService;
+    }
+
+    //DTO - Data Transfer Object - is an object that encapsulates some data and send it from one subsystem of the app to another (f.e. from client to api)
+
+    [HttpPost("register")] //POST: api/account/register
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
+        using var hmac = new HMACSHA512();  
+        //keyword "using" specifies that we won't need to hold the class that's being used in memory and 
+        //we can dispose of it after using it. When we are using "using" keyword, the class has to inherit
+        //from IDisposable interface so it has to contain Dispose() method, that will be called to remove it from the memory
+
+        var user = new AppUser
+        {
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
+    }
+
+    [HttpPost("login")] //GET: api/account/login
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(user => user.UserName == loginDto.Username);
+
+        if (user == null) return Unauthorized("Invalid username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for(int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await _context.Users.AnyAsync(user => user.UserName == username.ToLower());
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,8 +1,12 @@
-﻿namespace API.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities;
 
 //This class is an Entity. Entities represent tables in a db.
 public class AppUser    
 {
     public int Id { get; set; } //property named Id is automatically used as a primary key in this table
     public string UserName { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
 }
